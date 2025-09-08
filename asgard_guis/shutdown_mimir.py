8f2f6c3f9dc27eb4c996bb9ec2f7a9f488678f5f
ataras2 <ataras2@gmail.com>
2025-09-08 10:06:54 -0300

    added package scripts, needs installing

--- asgard_guis/shutdown_mimir.py
+++ asgard_guis/shutdown_mimir.py
@@ -1,36 +1,41 @@
 import PDU
 import time
 
-MIMIR_OUTLETS = [1, 8]
 
-pdu = PDU.AtenEcoPDU("192.168.100.11")
-pdu.connect()
+def main():
+    MIMIR_OUTLETS = [1, 8]
 
-input("Did you type sudo shutdown -h now on Mimir? Press Enter to continue...")
+    pdu = PDU.AtenEcoPDU("192.168.100.11")
+    pdu.connect()
 
-for outlet in MIMIR_OUTLETS:
-    pdu.switch_outlet_status(outlet, "off")
+    input("Did you type sudo shutdown -h now on Mimir? Press Enter to continue...")
 
-print("Powering off Mimir outlets...")
-
-
-is_off = [False for _ in MIMIR_OUTLETS]
-max_time = 60
-start_time = time.time()
-
-while (not all(is_off)) and (time.time() - start_time < max_time):
     for outlet in MIMIR_OUTLETS:
-        status = pdu.read_outlet_status(outlet)
-        if status == "off":
-            is_off[MIMIR_OUTLETS.index(outlet)] = True
-            print(f"Outlet {outlet} is off")
+        pdu.switch_outlet_status(outlet, "off")
 
-    time.sleep(1)
+    print("Powering off Mimir outlets...")
 
-if all(is_off):
-    print("Mimir is off")
-else:
-    print("ERROR: Mimir is not off, check PDU status manually")
-    for outlet, status in zip(MIMIR_OUTLETS, is_off):
-        if not status:
-            print(f"Outlet {outlet} is still on")
+    is_off = [False for _ in MIMIR_OUTLETS]
+    max_time = 60
+    start_time = time.time()
+
+    while (not all(is_off)) and (time.time() - start_time < max_time):
+        for outlet in MIMIR_OUTLETS:
+            status = pdu.read_outlet_status(outlet)
+            if status == "off":
+                is_off[MIMIR_OUTLETS.index(outlet)] = True
+                print(f"Outlet {outlet} is off")
+
+        time.sleep(1)
+
+    if all(is_off):
+        print("Mimir is off")
+    else:
+        print("ERROR: Mimir is not off, check PDU status manually")
+        for outlet, status in zip(MIMIR_OUTLETS, is_off):
+            if not status:
+                print(f"Outlet {outlet} is still on")
+
+
+if __name__ == "__main__":
+    main()
--- asgard_guis/startup_mimir.py
+++ asgard_guis/startup_mimir.py
@@ -1,31 +1,37 @@
 import PDU
 import time
 
-MIMIR_OUTLETS = [1, 8]
 
-pdu = PDU.AtenEcoPDU("192.168.100.11")
-pdu.connect()
+def main():
+    MIMIR_OUTLETS = [1, 8]
 
-for outlet in MIMIR_OUTLETS:
-    pdu.switch_outlet_status(outlet, "on")
+    pdu = PDU.AtenEcoPDU("192.168.100.11")
+    pdu.connect()
 
-print("Powering on Mimir outlets...")
-time.sleep(10)
-
-is_on = [False for _ in MIMIR_OUTLETS]
-start_time = time.time()
-max_time = 50
-
-while (not all(is_on)) and (time.time() - start_time < max_time):
     for outlet in MIMIR_OUTLETS:
-        status = pdu.read_outlet_status(outlet)
-        if status == "on":
-            is_on[MIMIR_OUTLETS.index(outlet)] = True
-            print(f"Outlet {outlet} is on")
+        pdu.switch_outlet_status(outlet, "on")
 
-    time.sleep(1)
+    print("Powering on Mimir outlets...")
+    time.sleep(10)
 
-if all(is_on):
-    print("Mimir is on, expecting 10 mins to boot")
-else:
-    print("ERROR: Mimir is not on, check PDU status manually")
+    is_on = [False for _ in MIMIR_OUTLETS]
+    start_time = time.time()
+    max_time = 50
+
+    while (not all(is_on)) and (time.time() - start_time < max_time):
+        for outlet in MIMIR_OUTLETS:
+            status = pdu.read_outlet_status(outlet)
+            if status == "on":
+                is_on[MIMIR_OUTLETS.index(outlet)] = True
+                print(f"Outlet {outlet} is on")
+
+        time.sleep(1)
+
+    if all(is_on):
+        print("Mimir is on, expecting 10 mins to boot")
+    else:
+        print("ERROR: Mimir is not on, check PDU status manually")
+
+
+if __name__ == "__main__":
+    main()
