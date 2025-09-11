6c5ef59b7360940b3b756ee156d848ab45b8f211
ataras2 <ataras2@gmail.com>
2025-09-11 19:40:15 -0300

    made search gui, needs testing

---
+++ asgard_guis/spiral_search.py
@@ -0,0 +1,36 @@
+# Base for spiral-search GUI in python
+
+import os
+
+
+class SpiralSearchIntegrator:
+    def __init__(self):
+        self.accumulated_offsets = {1: [0, 0], 2: [0, 0], 3: [0, 0], 4: [0, 0]}
+
+    def send_image_relative_offset(self, beam, x, y):
+        # Prepare offsets for each beam (1-4)
+        offsets = []
+        for i in range(1, 5):
+            if i == beam:
+                offsets.extend([str(x), str(y), "0", "0", "0"])
+            else:
+                offsets.extend(["0", "0", "0", "0", "0"])
+        # Build the command string
+        cmd = (
+            'msgSend wvgvlti issifControl OFFSACQ "'
+            + ",".join([f"{i},{','.join(offsets[(i-1)*5:i*5])}" for i in range(1, 5)])
+            + '"'
+        )
+
+        self.accumulated_offsets[beam][0] += x
+        self.accumulated_offsets[beam][1] += y
+
+        os.system(cmd)
+
+    def write_pointing_offsets_to_db(self):
+        # Write accumulated offsets into database for beams 0-3
+        for i in range(4):
+            x_offset = self.accumulated_offsets[i + 1][0]
+            y_offset = self.accumulated_offsets[i + 1][1]
+            os.system(f'dbWrite "<alias>mimir.hdlr_x_pof({i})" {x_offset}')
+            os.system(f'dbWrite "<alias>mimir.hdlr_y_pof({i})" {y_offset}')
---
+++ asgard_guis/spiral_search_gui.py
@@ -0,0 +1,77 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+from spiral_search import SpiralSearchIntegrator
+
+
+class SpiralSearchGUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Spiral Search Controller")
+        self.integrator = SpiralSearchIntegrator()
+        self.init_ui()
+
+    def init_ui(self):
+        layout = QtWidgets.QVBoxLayout()
+
+        # Beam selection
+        beam_layout = QtWidgets.QHBoxLayout()
+        beam_label = QtWidgets.QLabel("Select Beam:")
+        self.beam_combo = QtWidgets.QComboBox()
+        self.beam_combo.addItems([str(i) for i in range(1, 5)])
+        beam_layout.addWidget(beam_label)
+        beam_layout.addWidget(self.beam_combo)
+        layout.addLayout(beam_layout)
+
+        # Step size
+        step_layout = QtWidgets.QHBoxLayout()
+        step_label = QtWidgets.QLabel("Step Size:")
+        self.step_box = QtWidgets.QDoubleSpinBox()
+        self.step_box.setDecimals(2)
+        self.step_box.setValue(40.0)
+        self.step_box.setMinimum(0.01)
+        step_layout.addWidget(step_label)
+        step_layout.addWidget(self.step_box)
+        layout.addLayout(step_layout)
+
+        # X/Y controls
+        grid = QtWidgets.QGridLayout()
+        self.x_minus_btn = QtWidgets.QPushButton("-X")
+        self.x_plus_btn = QtWidgets.QPushButton("+X")
+        self.y_minus_btn = QtWidgets.QPushButton("-Y")
+        self.y_plus_btn = QtWidgets.QPushButton("+Y")
+        grid.addWidget(self.y_plus_btn, 0, 1)
+        grid.addWidget(self.x_minus_btn, 1, 0)
+        grid.addWidget(self.x_plus_btn, 1, 2)
+        grid.addWidget(self.y_minus_btn, 2, 1)
+        layout.addLayout(grid)
+
+        # Done button
+        self.done_btn = QtWidgets.QPushButton("Done")
+        layout.addWidget(self.done_btn)
+
+        self.setLayout(layout)
+
+        # Connect signals
+        self.x_minus_btn.clicked.connect(lambda: self.move_offset(-1, 0))
+        self.x_plus_btn.clicked.connect(lambda: self.move_offset(1, 0))
+        self.y_minus_btn.clicked.connect(lambda: self.move_offset(0, -1))
+        self.y_plus_btn.clicked.connect(lambda: self.move_offset(0, 1))
+        self.done_btn.clicked.connect(self.finish_and_close)
+
+    def move_offset(self, dx, dy):
+        beam = int(self.beam_combo.currentText())
+        step = self.step_box.value()
+        x = dx * step
+        y = dy * step
+        self.integrator.send_image_relative_offset(beam, x, y)
+
+    def finish_and_close(self):
+        self.integrator.write_pointing_offsets_to_db()
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    win = SpiralSearchGUI()
+    win.show()
+    sys.exit(app.exec_())
