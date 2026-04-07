a6db2e74c4309fd693507b294f46834d49a68001
Mike Ireland <michael.ireland@anu.edu.au>
2026-04-08 01:39:04 +1000

    Showing multiple network bandwidths now

--- asgard_guis/network_bw.py
+++ asgard_guis/network_bw.py
@@ -13,6 +13,7 @@ The program should run indefinitely until interrupted by the user.
 import sys
 import time
 import os
+import subprocess
 
 def get_tx_bytes(interface):
     """Read tx_bytes from sysfs for the given interface."""
@@ -22,6 +23,37 @@ def get_tx_bytes(interface):
             return long(f.read().strip())
     except (IOError, OSError):
         return None
+
+
+def get_remote_tx_bytes(remote_host, remote_path, tmp_file):
+    """Fetch tx_bytes from a remote host via rcp into a temp file, then read it."""
+    devnull = open(os.devnull, 'w')
+    try:
+        ret = subprocess.call(
+            ['rcp', remote_host + ':' + remote_path, tmp_file],
+            stdout=devnull, stderr=devnull
+        )
+    finally:
+        devnull.close()
+    if ret != 0:
+        return None
+    try:
+        with open(tmp_file, 'r') as f:
+            return long(f.read().strip())
+    except (IOError, OSError):
+        return None
+
+
+def format_line(label, delta):
+    """Format a single display line with label, rate, and bar."""
+    bar = draw_bar(delta, 100000000, 50)
+    if delta >= 100000000:
+        return '\033[1m%s: XXX,XXX kbytes/s %s\033[0m' % (label, bar)
+    elif delta >= 1000000:
+        return '\033[1m%s: %3d,%03d kbytes/s %s\033[0m' % (
+            label, delta // 1000000, (delta // 1000) % 1000, bar)
+    else:
+        return '\033[1m%s: %7d kbytes/s %s\033[0m' % (label, delta // 1000, bar)
 
 
 def draw_bar(value, max_value=100000000, width=50):
@@ -48,43 +80,44 @@ def draw_bar(value, max_value=100000000, width=50):
 
 def main():
     """Main program loop."""
-    interface = 'p3p1'
-    prev_bytes = 0
-    
+    local_interface = 'p3p1'
+    remote_host = 'wag'
+    remote_path = '/sys/class/net/p7p2/tx_bytes'
+    tmp_file = '/tmp/wag_tx_bytes'
+
+    prev_local = 0
+    prev_wag = 0
+
     try:
         while True:
-            current_bytes = get_tx_bytes(interface)
-            
-            if current_bytes is not None:
-                bytes_delta = current_bytes - prev_bytes
-                
-                # Ensure delta is non-negative
-                if bytes_delta < 0:
-                    bytes_delta = 0
-                
-                prev_bytes = current_bytes
-                
-                # Create bar graph (scale bytes for display)
-                bar = draw_bar(bytes_delta, 100000000, 50)
-                
-                # Print to terminal with carriage return for non-scrolling output
-                if bytes_delta >= 100000000:
-                    sys.stdout.write('\r\033[1mTX: XXX,XXX kbytes/s %s' % 
-                                (bar))
-                elif bytes_delta >= 1000000:
-                    sys.stdout.write('\r\033[1mTX: %3d,%03d kbytes/s %s' % 
-                                (bytes_delta//1000000, (bytes_delta/1000) % 1000, bar))
-                else:
-                    sys.stdout.write('\r\033[1mTX: %7d kbytes/s %s' % 
-                                (bytes_delta//1000, bar))
-                sys.stdout.flush()
+            current_local = get_tx_bytes(local_interface)
+            current_wag = get_remote_tx_bytes(remote_host, remote_path, tmp_file)
+
+            if current_local is not None:
+                delta_local = max(0, current_local - prev_local)
+                prev_local = current_local
+                line1 = format_line('Local TX', delta_local)
             else:
-                sys.stdout.write('\rError: Could not read from interface %s' % interface)
-                sys.stdout.flush()
-            
+                line1 = '\033[1mLocal TX: Error reading interface\033[0m'
+
+            if current_wag is not None:
+                delta_wag = max(0, current_wag - prev_wag)
+                prev_wag = current_wag
+                line2 = format_line('wag TX  ', delta_wag)
+            else:
+                line2 = '\033[1mwag TX  : Error reading remote\033[0m'
+
+            # Overwrite both lines in place using ANSI cursor control:
+            # clear & print line 1, drop to line 2, clear & print it,
+            # then move cursor back up to line 1 for the next iteration.
+            sys.stdout.write('\r\033[K' + line1)
+            sys.stdout.write('\n\r\033[K' + line2)
+            sys.stdout.write('\033[1A')
+            sys.stdout.flush()
+
             time.sleep(1)
     except KeyboardInterrupt:
-        sys.stdout.write('\n')
+        sys.stdout.write('\n\n')
         print("Program interrupted by user. Exiting.")
 
 
