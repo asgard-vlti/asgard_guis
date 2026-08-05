b54ba386371e277282629272c0371b424897855b
Marc White <mwhite1206@gmail.com>
2026-08-05 10:35:10 -0400

    First attempt at shutdown_mimir with shutdown command being automatically sent

--- asgard_guis/cmd_scripts/shutdown_mimir.py
+++ asgard_guis/cmd_scripts/shutdown_mimir.py
@@ -1,16 +1,70 @@
 import asgard_guis.PDU
 import time
+import subprocess
+import os
 
 # FIXME have these imported from somewhere common
 MIMIR_OUTLETS = [1, 8]
 PDU_IP_ADDRESS = "192.168.100.11"
+
+def _confirm_or_abort(message):
+    conf = input(message)
+    if conf == "y" or conf == "Y":
+        return True  # Continue
+    elif conf == "n" or conf == "N":
+        return False  # Abort
+    else:
+        print(f"Unable to interpret response '{conf}', please try again.")
+        return _confirm_or_abort(message)
 
 def main():
 
     pdu = asgard_guis.PDU.AtenEcoPDU(PDU_IP_ADDRESS)
     pdu.connect()
 
-    input("Did you type sudo shutdown -h now on Mimir? Press Enter to continue...")
+    proceed_to_poweroff = True
+
+    # Execute the shutdown command on mimir
+    # This requires passwordless sudo shutdown command to be active on mimir
+    # To do this, something like the following should be added to visudo:
+    # %group_name ALL=(ALL) NOPASSWD: /sbin/shutdown
+    # Gather the required env vars for the remsh command
+    rhost = os.getenv("RHOST", None)
+    ruser = os.getenv("RUSER", None)
+    display = os.getenv("DISPLAY", None)
+    # Execute the shutdown command
+    shutdown_result = None
+    if not any(_ is None for _ in [rhost, ruser, display]):
+        shutdown_result = subprocess.call([
+            "remsh",
+            f"{rhost}",
+            "-l", f"{ruser}",
+            "-n",
+            f"DISPLAY={display} xterm -e ssh -XC mimir sudo /usr/sbin/shutdown -h now 1>&- 2>&- &"
+        ])
+    else:
+        proceed_to_poweroff = _confirm_or_abort(
+            "WARNING: At least one env var required for the shutdown " \
+            "command to be sent is missing. This means this script " \
+            "cannot execute the shutdown command on mimir, and " \
+            "you will need to trigger that manually first. " \
+            "Do you will want to " \
+            "attempt the power off? (y/n):"
+        )
+
+    if proceed_to_poweroff and (
+        shutdown_result is None or shutdown_result != 0
+    ):
+        proceed_to_poweroff = _confirm_or_abort(
+            f"WARNING: Mimir shutdown may not have occurred "
+            f"(return code: {'n/a' if shutdown_result is None else shutdown_result.result}). " \
+            f"Either verify shutdown is proceeding and/or trigger manually and " \
+            f"then type Y, or type N to abort. (y/n):"
+        )
+
+    if not proceed_to_poweroff:
+        print("ERROR: Mimir power off has been aborted.")
+        return
 
     for outlet in MIMIR_OUTLETS:
         pdu.switch_outlet_status(outlet, "off")
