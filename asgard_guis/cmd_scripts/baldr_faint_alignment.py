915ccc6b0f12630898061eb1ef57d7090120e159
Mike Ireland <michael.ireland@anu.edu.au>
2026-09-12 07:25:25 +1000

    Re-arranged buttons

--- asgard_guis/cmd_scripts/baldr_faint_alignment.py
+++ asgard_guis/cmd_scripts/baldr_faint_alignment.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore, QtWidgets
 class BaldrFaintAlignmentGUI(QtWidgets.QWidget):
 	def __init__(self):
 		super().__init__()
+		self.move_delta = 1.0
 		self.setWindowTitle("Baldr Faint Alignment")
 		self._apply_dark_theme()
 		self._init_ui()
@@ -21,18 +22,29 @@ class BaldrFaintAlignmentGUI(QtWidgets.QWidget):
 		self.beam_combo.currentTextChanged.connect(self._update_align_button)
 		layout.addWidget(self.beam_combo, 0, 1)
 
-		self.spherical_checkbox = QtWidgets.QCheckBox("Spherical?")
-		layout.addWidget(self.spherical_checkbox, 0, 2, 1, 2)
+		layout.addWidget(QtWidgets.QLabel("Spherical?"), 0, 2)
+		self.spherical_checkbox = QtWidgets.QCheckBox()
+		layout.addWidget(self.spherical_checkbox, 0, 3)
+
+		self.move_delta_label = QtWidgets.QLabel()
+		self._update_move_delta_label()
+		layout.addWidget(self.move_delta_label, 0, 4, 1, 2)
 
 		self._add_direction_controls(layout, "Field Lens", 1, 0)
 		self._add_direction_controls(layout, "Pupil", 1, 3)
+		self.more_btn = QtWidgets.QPushButton("More")
+		self.more_btn.clicked.connect(lambda: self._change_move_delta(2))
+		layout.addWidget(self.more_btn, 2, 6)
+		self.less_btn = QtWidgets.QPushButton("Less")
+		self.less_btn.clicked.connect(lambda: self._change_move_delta(0.5))
+		layout.addWidget(self.less_btn, 3, 6)
 
 		self.align_btn = QtWidgets.QPushButton()
 		self._update_align_button(self.beam_combo.currentText())
-		layout.addWidget(self.align_btn, 4, 0, 1, 3)
+		layout.addWidget(self.align_btn, 6, 0, 1, 3)
 
 		self.save_btn = QtWidgets.QPushButton("Save")
-		layout.addWidget(self.save_btn, 4, 3, 1, 3)
+		layout.addWidget(self.save_btn, 6, 3, 1, 3)
 
 	def _add_direction_controls(self, layout, label, row, column):
 		layout.addWidget(QtWidgets.QLabel(label), row, column, 1, 3, QtCore.Qt.AlignCenter)
@@ -57,6 +69,13 @@ class BaldrFaintAlignmentGUI(QtWidgets.QWidget):
 	def _update_align_button(self, beam):
 		label = "Align CRed box" if beam == "1" else "Align BOTX"
 		self.align_btn.setText(label)
+
+	def _change_move_delta(self, factor):
+		self.move_delta *= factor
+		self._update_move_delta_label()
+
+	def _update_move_delta_label(self):
+		self.move_delta_label.setText(f"Move Delta: {self.move_delta:g}")
 
 	def _apply_dark_theme(self):
 		self.setStyleSheet(
@@ -87,6 +106,15 @@ class BaldrFaintAlignmentGUI(QtWidgets.QWidget):
 				background-color: #272a30;
 				color: #e6e6e6;
 				selection-background-color: #3f6db3;
+			}
+			QCheckBox::indicator {
+				width: 16px;
+				height: 16px;
+				border: 1px solid #8c919a;
+				background-color: #272a30;
+			}
+			QCheckBox::indicator:checked {
+				background-color: #3f6db3;
 			}
 			"""
 		)
